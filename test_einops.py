8db5684430988279b4fef62b5554424fcdf4ab8a
SlavkoPrytula <slavko.prytula@gmail.com>
2025-06-20 17:19:25 +0300

    cleanup

--- test_einops.py
+++ test_einops.py
@@ -189,32 +189,35 @@
 
 
 
-# import os
+import os
 
-# def print_dir_tree(root_dir, indent=""):
-#     """Recursively prints the directory tree structure."""
-#     try:
-#         entries = sorted(os.listdir(root_dir))
-#     except PermissionError:
-#         print(f"{indent}[Permission Denied] {root_dir}")
-#         return
+def print_dir_tree(root_dir, indent=""):
+    """Recursively prints the directory tree structure."""
+    try:
+        entries = sorted(os.listdir(root_dir))
+    except PermissionError:
+        print(f"{indent}[Permission Denied] {root_dir}")
+        return
     
-#     for index, entry in enumerate(entries):
-#         path = os.path.join(root_dir, entry)
-#         is_last = index == len(entries) - 1
-#         prefix = "└── " if is_last else "├── "
-        
-#         print(indent + prefix + entry)
-        
-#         if os.path.isdir(path):
-#             new_indent = indent + ("    " if is_last else "│   ")
-#             print_dir_tree(path, new_indent)
+    print(entries)
+    raise
+    for index, entry in enumerate(entries):
+        if 'runs' not in entry:
+            path = os.path.join(root_dir, entry)
+            is_last = index == len(entries) - 1
+            prefix = "└── " if is_last else "├── "
+            
+            print(indent + prefix + entry)
+            
+            if os.path.isdir(path):
+                new_indent = indent + ("    " if is_last else "│   ")
+                print_dir_tree(path, new_indent)
 
 
-# root_path = "./configs"
-# print_dir_tree(root_path)
+root_path = "./"
+print_dir_tree(root_path)
 
 
 
-data_dir = "/gpfs/helios/home/etais/com_palo/2025-03-06_OrganoidExport1"
+# data_dir = "/gpfs/helios/home/etais/com_palo/2025-03-06_OrganoidExport1"
 
